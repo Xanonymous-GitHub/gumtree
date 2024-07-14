c703b3242cf004b88f9a51d1de9c8d110646b3c6
Xanonymous <trusaidlin@gmail.com>
2024-07-14 23:52:44 +0100

    style: 💄 add ktlint integration tasks and format project

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.kotlinJvm) apply false
+    alias(libs.plugins.gradleKtlint)
 }
 
+internal val gradleKtLintPluginId = libs.plugins.gradleKtlint.get().pluginId
+
 allprojects {
+    apply(plugin = gradleKtLintPluginId)
+
     val localJavaVersion = JavaVersion.VERSION_21.majorVersion
 
     tasks.withType<KotlinCompile>().configureEach {
+        dependsOn(tasks.ktlintFormat)
         compilerOptions {
             jvmTarget.set(JvmTarget.fromTarget(localJavaVersion))
         }
