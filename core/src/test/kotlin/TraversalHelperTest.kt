05eacfa1d4948810acb9ecaa89c8df71b19e5075
Xanonymous <trusaidlin@gmail.com>
2024-07-21 03:48:31 +0100

    test: 💍 add test for traversal helpers

---
+++ core/src/test/kotlin/TraversalHelperTest.kt
@@ -0,0 +1,97 @@
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
+import tw.xcc.gumtree.helper.postOrdered
+import tw.xcc.gumtree.helper.preOrdered
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+
+@Execution(ExecutionMode.CONCURRENT)
+internal class TraversalHelperTest {
+    private lateinit var givenRoot: FakeTinnyTree
+
+    @BeforeTest
+    fun setUp() {
+        givenRoot = FakeTinnyTree()
+    }
+
+    @Test
+    fun `test preOrdered`() {
+        val child1 = FakeTinnyTree()
+        val child2 = FakeTinnyTree()
+        val child3 = FakeTinnyTree()
+        val child4 = FakeTinnyTree()
+
+        givenRoot.addChild(child1)
+        child1.addChild(child2)
+        child1.addChild(child3)
+        givenRoot.addChild(child4)
+
+        val actualPreOrdered = mutableListOf<FakeTinnyTree>()
+        preOrdered(givenRoot) { actualPreOrdered.add(it) }
+
+        val expectedPreOrdered = listOf(givenRoot, child1, child2, child3, child4)
+        assertContentEquals(expectedPreOrdered, actualPreOrdered)
+    }
+
+    @Test
+    fun `test preOrdered with empty tree`() {
+        val actualPreOrdered = mutableListOf<FakeTinnyTree>()
+        preOrdered(givenRoot) { actualPreOrdered.add(it) }
+
+        val expectedPreOrdered = listOf(givenRoot)
+        assertContentEquals(expectedPreOrdered, actualPreOrdered)
+    }
+
+    @Test
+    fun `test preOrdered with single child`() {
+        val child1 = FakeTinnyTree()
+        givenRoot.addChild(child1)
+
+        val actualPreOrdered = mutableListOf<FakeTinnyTree>()
+        preOrdered(givenRoot) { actualPreOrdered.add(it) }
+
+        val expectedPreOrdered = listOf(givenRoot, child1)
+        assertContentEquals(expectedPreOrdered, actualPreOrdered)
+    }
+
+    @Test
+    fun `test postOrdered`() {
+        val child1 = FakeTinnyTree()
+        val child2 = FakeTinnyTree()
+        val child3 = FakeTinnyTree()
+        val child4 = FakeTinnyTree()
+
+        givenRoot.addChild(child1)
+        child1.addChild(child2)
+        child1.addChild(child3)
+        givenRoot.addChild(child4)
+
+        val actualPostOrdered = mutableListOf<FakeTinnyTree>()
+        postOrdered(givenRoot) { actualPostOrdered.add(it) }
+
+        val expectedPostOrdered = listOf(child2, child3, child1, child4, givenRoot)
+        assertContentEquals(expectedPostOrdered, actualPostOrdered)
+    }
+
+    @Test
+    fun `test postOrdered with empty tree`() {
+        val actualPostOrdered = mutableListOf<FakeTinnyTree>()
+        postOrdered(givenRoot) { actualPostOrdered.add(it) }
+
+        val expectedPostOrdered = listOf(givenRoot)
+        assertContentEquals(expectedPostOrdered, actualPostOrdered)
+    }
+
+    @Test
+    fun `test postOrdered with single child`() {
+        val child1 = FakeTinnyTree()
+        givenRoot.addChild(child1)
+
+        val actualPostOrdered = mutableListOf<FakeTinnyTree>()
+        postOrdered(givenRoot) { actualPostOrdered.add(it) }
+
+        val expectedPostOrdered = listOf(child1, givenRoot)
+        assertContentEquals(expectedPostOrdered, actualPostOrdered)
+    }
+}
