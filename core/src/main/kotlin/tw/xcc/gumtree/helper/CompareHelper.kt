05c4b12371b6e052d6ed68906d9764838c5cdff6
Xanonymous <trusaidlin@gmail.com>
2024-07-21 07:28:02 +0100

    perf: ⚡️ refactor compare helper to concurrent by coroutine

--- core/src/main/kotlin/tw/xcc/gumtree/helper/CompareHelper.kt
+++ core/src/main/kotlin/tw/xcc/gumtree/helper/CompareHelper.kt
@@ -1,46 +1,67 @@
 package tw.xcc.gumtree.helper
 
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.runBlocking
 import tw.xcc.gumtree.model.GumTree
 
 fun isIsomorphicTo(
     self: GumTree,
     other: GumTree
 ): Boolean =
-    compareTrees(self, other, true) { child, otherChild ->
-        child isIsomorphicTo otherChild
+    runBlocking {
+        compareTrees(self, other, true)
     }
 
 fun isIsoStructuralTo(
     self: GumTree,
     other: GumTree
 ): Boolean =
-    compareTrees(self, other, false) { child, otherChild ->
-        child isIsoStructuralTo otherChild
+    runBlocking {
+        compareTrees(self, other, false)
     }
 
-private inline fun compareTrees(
+private suspend fun compareTrees(
     self: GumTree,
     other: GumTree,
-    shouldCheckLabel: Boolean,
-    childComparison: (GumTree, GumTree) -> Boolean
-): Boolean {
-    synchronized(self) {
-        if (!(self hasSameTypeAs other && self.childCount() == other.childCount())) {
-            return false
+    shouldCheckLabel: Boolean
+): Boolean =
+    coroutineScope {
+        val selfChildCount = self.childCount()
+        if (!(self hasSameTypeAs other && selfChildCount == other.childCount())) {
+            return@coroutineScope false
         }
 
         if (shouldCheckLabel && !(self hasSameLabelAs other)) {
-            return false
+            return@coroutineScope false
         }
 
-        for (i in 0 until self.childCount()) {
-            val child = self.childAt(i) ?: return false
-            val otherChild = other.childAt(i) ?: return false
-            if (!childComparison(child, otherChild)) {
-                return false
+        if (selfChildCount == 0) {
+            return@coroutineScope true
+        }
+
+        val selfChildren = self.getChildren()
+        val otherChildren = other.getChildren()
+
+        val jobs = mutableListOf<Deferred<Boolean>>()
+        selfChildren.zip(otherChildren).forEach { (selfChild, otherChild) ->
+            val job =
+                async(Dispatchers.Default) {
+                    println(coroutineContext)
+                    ensureActive()
+                    compareTrees(selfChild, otherChild, shouldCheckLabel)
+                }
+            jobs.add(job)
+        }
+
+        jobs.forEach {
+            if (!it.await()) {
+                return@coroutineScope false
             }
         }
 
-        return true
+        return@coroutineScope true
     }
-}
