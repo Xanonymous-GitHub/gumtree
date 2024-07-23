d8db82b39abcfe87f7210a7bd7e915e2802e8767
Xanonymous <trusaidlin@gmail.com>
2024-07-23 12:12:02 +0100

    refactor: 💡 change memo hash type to Long

--- core/src/main/kotlin/tw/xcc/gumtree/helper/HashMemoHelper.kt
+++ core/src/main/kotlin/tw/xcc/gumtree/helper/HashMemoHelper.kt
@@ -7,20 +7,21 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 import tw.xcc.gumtree.model.BasicTree
+import java.nio.ByteBuffer
 import java.util.concurrent.ConcurrentHashMap
 
 private const val SEED = 0x0L
 private val xxh364 = XXH3_64.Seeded(SEED)
 
-fun <T : BasicTree<T>> createHashMemoOf(tree: T): Map<String, List<Byte>> {
-    val memo = ConcurrentHashMap<String, List<Byte>>()
+fun <T : BasicTree<T>> createHashMemoOf(tree: T): Map<String, Long> {
+    val memo = ConcurrentHashMap<String, Long>()
     runBlocking(Dispatchers.Default) { createHashMemoOf(tree, memo) }
     return memo
 }
 
 private suspend fun <T : BasicTree<T>> createHashMemoOf(
     tree: T,
-    memo: MutableMap<String, List<Byte>>
+    memo: MutableMap<String, Long>
 ): ByteArray =
     coroutineScope {
         val digest = xxh364.createDigest()
@@ -34,6 +35,8 @@ private suspend fun <T : BasicTree<T>> createHashMemoOf(
         }
 
         val hash = digest.digest(tree.similarityProperties().toByteArray())
-        memo[tree.id] = hash.toList()
+        memo[tree.id] = hash.toLong()
         return@coroutineScope hash
     }
+
+private fun ByteArray.toLong(): Long = ByteBuffer.wrap(this).long
