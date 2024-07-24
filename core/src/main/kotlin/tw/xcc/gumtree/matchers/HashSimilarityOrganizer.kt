3e4b110d8449fd662979d7069181981fd6ad2dfa
Xanonymous <trusaidlin@gmail.com>
2024-07-24 02:32:53 +0100

    feat: 🎸 add hash similarity organizer

---
+++ core/src/main/kotlin/tw/xcc/gumtree/matchers/HashSimilarityOrganizer.kt
@@ -0,0 +1,63 @@
+package tw.xcc.gumtree.matchers
+
+import tw.xcc.gumtree.model.GumTree
+
+private typealias IsomorphicTreesTypeInternal = Pair<MutableSet<GumTree>, MutableSet<GumTree>>
+internal typealias IsomorphicTreesType = Pair<Set<GumTree>, Set<GumTree>>
+internal typealias MappingsType = List<Pair<GumTree, GumTree>>
+
+class HashSimilarityOrganizer(
+    memo1: Map<String, Long>,
+    memo2: Map<String, Long>,
+    mappings: MappingsType
+) {
+    private val hashToTreePairs = mutableMapOf<Long, IsomorphicTreesTypeInternal>()
+
+    val uniqueIsomorphicMappings: Sequence<IsomorphicTreesType> by lazy {
+        hashToTreePairs.values.asSequence().filter {
+            it.first.size == 1 && it.second.size == 1
+        }
+    }
+
+    val nonUniqueIsomorphicMappings: Sequence<IsomorphicTreesType> by lazy {
+        hashToTreePairs.values.asSequence().filter {
+            (it.first.size > 1 && it.second.size >= 1) || (it.first.size >= 1 && it.second.size > 1)
+        }
+    }
+
+    val nonIsomorphicMappings: Sequence<IsomorphicTreesType> by lazy {
+        hashToTreePairs.values.asSequence().filter {
+            it.first.size == 0 || it.second.size == 0
+        }
+    }
+
+    private fun newIsomorphicMappings(
+        memo1: Map<String, Long>,
+        memo2: Map<String, Long>,
+        mappings: MappingsType
+    ) {
+        mappings.forEach { mapping ->
+            val hashOfLeft = memo1[mapping.first.id]
+            val hashOfRight = memo2[mapping.second.id]
+            if (hashOfLeft == null || hashOfRight == null) {
+                throw NoSuchElementException("The hash of the tree should be existed in its memo.")
+            }
+
+            if (hashToTreePairs.containsKey(hashOfLeft)) {
+                hashToTreePairs[hashOfLeft]!!.first.add(mapping.first)
+            } else {
+                hashToTreePairs[hashOfLeft] = IsomorphicTreesTypeInternal(mutableSetOf(mapping.first), mutableSetOf())
+            }
+
+            if (hashToTreePairs.containsKey(hashOfRight)) {
+                hashToTreePairs[hashOfRight]!!.second.add(mapping.second)
+            } else {
+                hashToTreePairs[hashOfRight] = IsomorphicTreesTypeInternal(mutableSetOf(), mutableSetOf(mapping.second))
+            }
+        }
+    }
+
+    init {
+        newIsomorphicMappings(memo1, memo2, mappings)
+    }
+}
