3c8ee617683eccdf6fac15939392a6e14c57bdab
Xanonymous <trusaidlin@gmail.com>
2024-07-24 15:24:58 +0100

    perf: ⚡️ add contract for compiler optimization

--- core/src/main/kotlin/tw/xcc/gumtree/helper/GumTreeCreatorHelper.kt
+++ core/src/main/kotlin/tw/xcc/gumtree/helper/GumTreeCreatorHelper.kt
@@ -2,10 +2,14 @@ package tw.xcc.gumtree.helper
 
 import tw.xcc.gumtree.model.GumTree
 import tw.xcc.gumtree.model.TreeType
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 @DslMarker
 internal annotation class GumTreeMarker
 
+@OptIn(ExperimentalContracts::class)
 @GumTreeMarker
 internal class GumTreeBuilder(name: String, label: String) {
     private val root by lazy { GumTree(TreeType(name), label) }
@@ -15,6 +19,10 @@ internal class GumTreeBuilder(name: String, label: String) {
         label: String,
         block: GumTreeBuilder.() -> Unit
     ) {
+        contract {
+            callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+        }
+
         val child = GumTreeBuilder(name, label)
         child.block()
         root.addChild(child.build())
@@ -30,11 +38,16 @@ internal class GumTreeBuilder(name: String, label: String) {
     fun build(): GumTree = root
 }
 
+@OptIn(ExperimentalContracts::class)
 internal fun gumTree(
     name: String,
     label: String,
     block: GumTreeBuilder.() -> Unit
 ): GumTree {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
     val builder = GumTreeBuilder(name, label)
     builder.block()
     return builder.build()
--- core/src/main/kotlin/tw/xcc/gumtree/helper/TraversalHelper.kt
+++ core/src/main/kotlin/tw/xcc/gumtree/helper/TraversalHelper.kt
@@ -1,11 +1,19 @@
 package tw.xcc.gumtree.helper
 
 import tw.xcc.gumtree.api.tree.Tree
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
+@OptIn(ExperimentalContracts::class)
 private fun <T : Tree> preOrderedImpl(
     tree: T,
     action: (T) -> Unit
 ) {
+    contract {
+        callsInPlace(action, InvocationKind.AT_LEAST_ONCE)
+    }
+
     action(tree)
     tree.getChildren().forEach {
         @Suppress("UNCHECKED_CAST")
@@ -13,10 +21,15 @@ private fun <T : Tree> preOrderedImpl(
     }
 }
 
+@OptIn(ExperimentalContracts::class)
 private fun <T : Tree> postOrderedImpl(
     tree: T,
     action: (T) -> Unit
 ) {
+    contract {
+        callsInPlace(action, InvocationKind.AT_LEAST_ONCE)
+    }
+
     tree.getChildren().forEach {
         @Suppress("UNCHECKED_CAST")
         postOrderedImpl(it as T, action)
