b20473a0fc5a0a4c51bd0ce31bbc81a5ed18ed41
Xanonymous <trusaidlin@gmail.com>
2024-07-27 09:44:23 +0100

    test: 💍 add test for the cache pool

---
+++ core/src/test/kotlin/NonFrozenGumTreePoolTest.kt
@@ -0,0 +1,92 @@
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
+import tw.xcc.gumtree.helper.gumTree
+import tw.xcc.gumtree.model.NonFrozenGumTreeCachePool
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertSame
+
+@Execution(ExecutionMode.SAME_THREAD)
+internal class NonFrozenGumTreePoolTest {
+    private val givenPool = NonFrozenGumTreeCachePool
+
+    @BeforeTest
+    fun setUp() {
+        givenPool.clear()
+    }
+
+    @Test
+    fun `test putAll`() {
+        val origin =
+            gumTree("root") {
+                child("child")
+            }
+
+        givenPool.putAll(origin.preOrdered().associateBy { it.id })
+        assertEquals(2, givenPool.size)
+
+        val extracted = givenPool[origin.id]
+        assertSame(extracted, origin)
+    }
+
+    @Test
+    fun `test put`() {
+        val origin =
+            gumTree("root") {
+                child("child")
+            }
+
+        givenPool[origin.id] = origin
+        assertEquals(1, givenPool.size)
+
+        val extracted = givenPool[origin.id]
+        assertSame(extracted, origin)
+    }
+
+    @Test
+    fun `test putAll with GumTreeView`() {
+        val origin =
+            gumTree("root") {
+                child("child")
+            }.toNewFrozen()
+
+        assertThrows<IllegalArgumentException> {
+            givenPool.putAll(origin.preOrdered().associateBy { it.id })
+        }
+    }
+
+    @Test
+    fun `test put with GumTreeView`() {
+        val origin =
+            gumTree("root") {
+                child("child")
+            }.toNewFrozen()
+
+        assertThrows<IllegalArgumentException> {
+            givenPool[origin.id] = origin
+        }
+    }
+
+    @Test
+    fun `test mustExtractRealOf`() {
+        val originL =
+            gumTree("rootL") {
+                child("childL")
+            }
+        val originR =
+            gumTree("rootR") {
+                child("childR")
+            }
+
+        givenPool.putAll(originL.preOrdered().associateBy { it.id })
+        givenPool.putAll(originR.preOrdered().associateBy { it.id })
+
+        val pair = originL to originR
+        val extracted = givenPool.mustExtractRealOf(pair)
+
+        assertSame(originL, extracted.first)
+        assertSame(originR, extracted.second)
+    }
+}
