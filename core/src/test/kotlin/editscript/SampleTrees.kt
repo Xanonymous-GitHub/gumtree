02a4a20985a8650ef5899d0fcec84bdf7d7a0d57
Xanonymous <trusaidlin@gmail.com>
2024-08-01 07:10:10 +0100

    test: 💍 add sample tree 1 and 2 for editscript testing

---
+++ core/src/test/kotlin/editscript/SampleTrees.kt
@@ -0,0 +1,74 @@
+package editscript
+
+import helpers.gumTree
+
+/**
+ * Sample tree 1 for test.
+ * Mentioned in Chawathe's paper Figure 2.
+ * */
+private val sampleTree1L =
+    gumTree("1") {
+        child("2")
+        child("3") {
+            child("5")
+        }
+        child("4")
+    }
+
+/**
+ * Sample tree 1 for test.
+ * Mentioned in Chawathe's paper Figure 2.
+ * */
+private val sampleTree1R =
+    gumTree("1") {
+        child("2")
+        child("4")
+        child("3") {
+            child("5")
+            child("6")
+        }
+    }
+
+/**
+ * Sample tree 2 for test.
+ * Mentioned in Chawathe's paper Figure 3.
+ * */
+private val sampleTree2L =
+    gumTree("1", "D") {
+        child("2", "S(1)")
+        child("3", "Sec") {
+            child("4", "S(x)")
+            child("5", "P") {
+                child("6", "S(a)")
+                child("7", "S(b)")
+            }
+        }
+        child("8", "P") {
+            child("9", "S(m)")
+            child("10", "S(n)")
+        }
+    }
+
+/**
+ * Sample tree 2 for test.
+ * Mentioned in Chawathe's paper Figure 3.
+ * */
+private val sampleTree2R =
+    gumTree("1", "D") {
+        child("3", "Sec") {
+            child("4", "S(x)")
+        }
+        child("8", "P") {
+            child("9", "S(m)")
+            child("10", "S(n)")
+        }
+        child("11", "Sec(foo)") {
+            child("5", "P") {
+                child("6", "S(a)")
+                child("7", "S(b)")
+            }
+        }
+    }
+
+val sampleTree1 = sampleTree1L to sampleTree1R
+val sampleTree2 = sampleTree2L to sampleTree2R
