58847bbb0415ed003bed488afb8af9bf5d45ff9b
Xanonymous <trusaidlin@gmail.com>
2024-08-06 11:51:54 +0100

    fix: 🐛 TreeType should also be serializalbe

--- core/src/main/kotlin/tw/xcc/gumtree/model/TreeType.kt
+++ core/src/main/kotlin/tw/xcc/gumtree/model/TreeType.kt
@@ -1,10 +1,12 @@
 package tw.xcc.gumtree.model
 
+import java.io.Serializable
+
 /**
  * TreeType corresponds to the name of their production rule in the grammar of an AST.
  * e.g. `IfStatement`, `FunctionStatement`
  * */
-data class TreeType(val name: String) {
+data class TreeType(val name: String) : Serializable {
     fun isEmpty(): Boolean = name.isEmpty()
 
     companion object {
