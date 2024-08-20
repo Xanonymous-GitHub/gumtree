82f166729c50d636d21c7ad93fe3eb34ca999539
Xanonymous <trusaidlin@gmail.com>
2024-08-20 20:26:17 +0100

    test: 💍 add jacoco config for antlr-bridge

--- antlr-bridge/build.gradle.kts
+++ antlr-bridge/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    jacoco
+}
+
 dependencies {
     implementation(project(":core"))
     implementation(libs.antlr)
@@ -7,4 +11,17 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core.jvm)
     testImplementation(libs.kotlin.test)
     testImplementation(libs.kotlin.test.junit5)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+        csv.required = true
+        html.required = true
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
 }
