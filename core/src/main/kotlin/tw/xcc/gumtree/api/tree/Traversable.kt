405e659231f277d9f10390ec6b2ad71b6d9b9e4b
Xanonymous <trusaidlin@gmail.com>
2024-08-21 12:41:43 +0100

    style: 💄 remove unused comments

--- core/src/main/kotlin/tw/xcc/gumtree/api/tree/Traversable.kt
+++ core/src/main/kotlin/tw/xcc/gumtree/api/tree/Traversable.kt
@@ -13,13 +13,4 @@ internal interface Traversable<out E> where E : Tree, E : Any {
      * Traverse the tree in post-order. This means that the current node is visited after its children.
      * */
     fun postOrdered(): List<E>
-
-    /**
-     * Traverse the tree in in-order. This means that the current node is visited between its children.
-     * Note that not every tree has a meaningful in-order traversal,
-     * since it requires how we define the left and right children.
-     * For example, a binary tree has a meaningful in-order traversal, but a general tree does not.
-     * So the default implementation is to return an empty list.
-     * */
-    // fun inOrdered(): List<E> = emptyList()
 }
